655ea12bba31b1918ba4695de6825ab9c6e7e42f
Lozumi Everkine <fantasynico1026@gmail.com>
2023-11-18 14:15:21 +0800

    initial: Initial the project

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # FileManager
 A java project for exam 2.
+
+## Function
+
+![uml.jpg](src%2Fmain%2Fresources%2Fuml.jpg)
+
+## Todo
+### Classes
+- [ ] FileSystem
+- [ ] Folder
+- [ ] FolderItem
+- [ ] FIle
+- [ ] Owner
+
+### Interface
+- [ ] java.long.Iterable<FolderItem>
+
+### Process
+- [ ] Build classes
+- [ ] Test
+- [ ] Write Javadoc
---
+++ src/main/java/Main.java
@@ -0,0 +1,21 @@
+import java.io.IOException;
+
+/**
+ * 主类
+ *
+ * <p>系统的主方法。
+ *
+ * @author Lozumi
+ * @version 1.0
+ */
+public class Main {
+    /**
+     * 主方法，调用子系统的主方法。
+     *
+     * @param args 传递给主方法的参数
+     * @throws IOException 当 IO 操作不受支持时抛出该异常
+     */
+    public static void main(String[] args) throws IOException {
+        FileSystem.main(args); // 调用子系统的主方法。
+    }
+}
